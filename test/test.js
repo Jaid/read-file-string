3bb73da575399c41361900cfa80c7768dc96f2e4
Jaid <jaid.jsx@gmail.com>
2019-12-19 21:50:44 +0100

    Improved tests
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -8,23 +8,39 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
 const {default: readFileString, readFileStringSync} = indexModule
 
 it("should return string (async)", async () => {
-  const result = await readFileString(path.join(__dirname, "..", "readme.md"))
+  const file = path.join(__dirname, "..", "readme.md")
+  const result = await readFileString(file)
   expect(typeof result.length).toBe("number")
   expect(result.length).toBeGreaterThan(10)
 })
 
-it("should return string (async)", async () => {
-  const result = await readFileString(path.join(__dirname, "..", "readmeNOT.md"))
+it("should return null for non-existing files (async)", async () => {
+  const file = path.join(__dirname, "..", "readmeNOT.md")
+  const result = await readFileString(file)
+  expect(result).toBe(null)
+})
+
+it("should return null for folders (async)", async () => {
+  const file = path.join(__dirname, "..", "src")
+  const result = await readFileString(file)
   expect(result).toBe(null)
 })
 
 it("should return string (sync)", () => {
-  const result = readFileStringSync(path.join(__dirname, "..", "readme.md"))
+  const file = path.join(__dirname, "..", "readme.md")
+  const result = readFileStringSync(file)
   expect(typeof result.length).toBe("number")
   expect(result.length).toBeGreaterThan(10)
 })
 
-it("should return string (sync)", () => {
-  const result = readFileStringSync(path.join(__dirname, "..", "readmeNOT.md"))
+it("should return null for non-existing files (sync)", () => {
+  const file = path.join(__dirname, "..", "readmeNOT.md")
+  const result = readFileStringSync(file)
+  expect(result).toBe(null)
+})
+
+it("should return null for folders (sync)", async () => {
+  const file = path.join(__dirname, "..", "src")
+  const result = await readFileStringSync(file)
   expect(result).toBe(null)
 })
